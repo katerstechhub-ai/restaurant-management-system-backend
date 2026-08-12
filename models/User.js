997cb1ef063d95201fef6982052fd4ed34b7fdaa
Kater <Katerstechhub@gmail.com>
2026-08-12 22:32:50 +0100

    wallet

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['admin', 'staff', 'customer'], default: 'customer' },
+  walletBalance: { type: Number, default: 0 },
   resetPasswordToken: { type: String },
   resetPasswordExpires: { type: Date },
 }, { timestamps: true });
